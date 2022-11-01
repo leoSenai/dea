5b3f226c4bade82144dfccd99bb1b3fe887b1473
Rafael <88169277+ShadeHonk@users.noreply.github.com>
2022-10-31 21:56:03 -0300

    create vue com quasar

---
+++ AppMotivar/src/components/EssentialLink.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    target="_blank"
+    :href="link"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>{{ caption }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+
+<script>
+import { defineComponent } from 'vue'
+import { useQuasar } from 'quasar'
+export default defineComponent({
+  name: 'EssentialLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    caption: {
+      type: String,
+      default: ''
+    },
+
+    link: {
+      type: String,
+      default: '#'
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    },
+    setup () {
+    const $q = useQuasar()
+
+    // calling here; equivalent to when component is created
+    $q.dark.set(true)
+  }
+  }
+})
+</script>

f7f34ee7f7c47a9dbec1f951b7f1fadf12f02c42
dariokrugerjunior <krugerdario1@gmail.com>
2023-05-03 19:43:14 -0300

    Removendo backend antigo

--- Backend/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import json
-
-app = Flask(__name__)
-
-@app.route("/")
-def root():
-    return json.dumps('Backend of DEA')
--- Backend/entities/questionario.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-sys.path.append('/baseClasses')
-from database import DB

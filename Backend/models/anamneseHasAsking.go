e82ef3d40980a8e5ab4dff3eac8bfd7cc6481a37
RodrigoDestri <rodrigodperozin@gmail.com>
2023-05-18 13:14:07 -0300

    chore: rules specified in anamneseHasAsking

--- Backend/models/anamneseHasAsking.go
+++ Backend/models/anamneseHasAsking.go
@@ -9,3 +9,13 @@ type AnamneseHasAsking struct {
 func (p *AnamneseHasAsking) TableName() string {
 	return "anamnese_has_pergunta"
 }
+
+/*
+
+Business rules:
+
+1 - When deleting an anamneseHasAsking register, the only parameter necessary is the 'IdAnamnese'.
+2 - This table should be used to verify if a asking can be deleted or not.
+3 - Must be able to get the records passing IdAdamnese or IdAsking as a parameter.
+
+*/

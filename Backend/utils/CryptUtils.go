dbf598d62ff83bef2ebc02a5907d4252270224cf
RodrigoDestri <rodrigodperozin@gmail.com>
2023-05-31 12:55:52 -0300

    feat: Generate salt function

--- Backend/utils/CryptUtils.go
+++ Backend/utils/CryptUtils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
 )
@@ -12,4 +13,13 @@ func GenerateSha256(password string) (sha256password string) {
 	sha256password = hex.EncodeToString(sha256o.Sum(nil))
 
 	return sha256password
+}
+
+func GenerateSalt() (salt string) {
+
+	saltByte := make([]byte, 15, 20)
+	rand.Read(saltByte)
+	salt = hex.EncodeToString(saltByte)
+
+	return salt
 }

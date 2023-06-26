f020809a484929490ecf89dd7b9b1d46af16eec8
Felipe Augusto Janotte <felipe.janotte@fulltechpro.com.br>
2023-06-26 19:42:51 -0300

    fix/routes

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <Header :links="links" />
+  <router-view name="header" />
   <router-view />
 </template>
 
 <script setup>
-import Header from './components/Header.vue';
-import { PhUsers } from '@phosphor-icons/vue';
 
-const links = [
-  // { path: '/pacientes', name: 'Pacientes', icon: PhScooter },
-  // { path: '/questionarios', name: 'Questionários', icon: PhArticle },
-  { path: '/usuarios', name: 'Usuários', icon: PhUsers }
-]
 </script>
 
 <style scoped></style>
--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,6 +1,33 @@
-import Home from '../views/Home.vue'
+import Home from '../views/Home.vue';
+import Header from '../components/Header.vue';
+
+import { PhUsers } from '@phosphor-icons/vue';
+
+const links = [
+  // { path: '/pacientes', name: 'Pacientes', icon: PhScooter },
+  // { path: '/questionarios', name: 'Questionários', icon: PhArticle },
+  {path: '/usuarios', name: 'Usuários', icon: PhUsers},
+];
 
 export const routes = [
-  { path: '/', component: Home, name: 'Home' },
-  { path: '/home', component: Home, name: 'Home' }
-]
+  {
+    path: '/',
+    components: {
+      default: Home,
+      header: Header,
+    },
+    props: {
+      header: { links },
+    },
+  },
+    {
+    path: '/home',
+    components: {
+      default: Home,
+      header: Header,
+    },
+    props: {
+      header: { links },
+    },
+  },
+];

f4d2fd16aa5b75aed63965be9739eee4ef3d3c94
dariokrugerjunior <krugerdario1@gmail.com>
2023-06-28 20:16:28 -0300

    feat: implementação do cadastro da proximidade

--- Backend/service/PersonService.go
+++ Backend/service/PersonService.go
@@ -25,7 +25,7 @@ func PostPerson(personDto dtos.PersonDTO) (err error) {
 			return fmt.Errorf("Não foi possivel cadastrar está pessoa!")
 		}
 
-		if personDto.IdPatient != 0 {
+		if personDto.IdPatient != 0 && personDto.DescPerson != "" {
 			proximity := models.Proximity{IdPatient: personDto.IdPatient, IdPerson: person.IdPerson, Desc: personDto.DescPerson}
 			err = repository.PostProximity(proximity)
 			if err != nil {
@@ -42,7 +42,7 @@ func GetAllPerson() (persons []models.Person, err error) {
 	return persons, err
 }
 
-func PutPerson(personUpdate models.Person) (err error) {
+func PutPerson(personUpdate dtos.PersonDTO) (err error) {
 	var person models.Person
 
 	person, err = repository.GetPersonById(personUpdate.IdPerson)
@@ -54,7 +54,21 @@ func PutPerson(personUpdate models.Person) (err error) {
 		return fmt.Errorf("Pessoa não está cadastrada no sistema!")
 	}
 
-	err = repository.PutPerson(personUpdate)
+	person = utils.ConvertPersonDTOToPerson(personUpdate)
+
+	err = repository.PutPerson(person)
+
+	if err != nil {
+		return err
+	}
+
+	if personUpdate.IdPatient != 0 && personUpdate.DescPerson != "" {
+		proximity := models.Proximity{IdPatient: personUpdate.IdPatient, IdPerson: personUpdate.IdPerson, Desc: personUpdate.DescPerson}
+		err = repository.PostProximity(proximity)
+		if err != nil {
+			return fmt.Errorf("Não foi possivel cadastrar a proximidade!")
+		}
+	}
 
 	return
 }

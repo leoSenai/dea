142e4c37749142530c93d6cc829e2dcdbbe76aa0
dariokrugerjunior <79287478+dariokrugerjunior@users.noreply.github.com>
2023-06-28 19:24:14 -0300

    Update tests.go

--- Backend/tests/tests.go
+++ Backend/tests/tests.go
@@ -10,9 +10,4 @@ func Run() {
 	//Tests is a module where you can run commands to test specific functions
 	//or procedures, use and abuse of tests to make sure that the system wil work fine.
 
-	exist1 := repositoryUtils.VerifyUserExistanceByDocument(0)
-	exist2 := repositoryUtils.VerifyUserExistanceByDocument(1)
-
-	fmt.Println(exist1, exist2)
-
 }

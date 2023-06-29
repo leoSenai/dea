7430f5d79abbf4ed5e1844eccc4cd340aa5263f4
Danilo de Matos Serafim <danilo442008@gmail.com>
2023-06-28 21:14:31 -0300

    Início da página de pacientes

--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,12 +1,13 @@
 import Home from '../views/HomeView.vue';
 import Header from '../components/HeaderPrimary.vue';
+import Patients from '../views/PatientsView.vue';
 
 import { PhUsers } from '@phosphor-icons/vue';
 
 const links = [
   // { path: '/pacientes', name: 'Pacientes', icon: PhScooter },
   // { path: '/questionarios', name: 'Questionários', icon: PhArticle },
-  {path: '/usuarios', name: 'Usuários', icon: PhUsers},
+  {path: '/patients', name: 'Pacientes', icon: PhUsers},
 ];
 
 export const routes = [
@@ -24,6 +25,16 @@ export const routes = [
     path: '/home',
     components: {
       default: Home,
+      header: Header,
+    },
+    props: {
+      header: { links },
+    },
+  },
+  {
+    path: '/patients',
+    components: {
+      default: Patients,
       header: Header,
     },
     props: {

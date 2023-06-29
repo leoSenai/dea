ee8e890dc30769af87e869fff3f22bfdb4031407
Felipe Augusto Janotte <felipe.janotte@fulltechpro.com.br>
2023-06-29 20:20:38 -0300

    home no content

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -1,21 +1,11 @@
 <template>
-  <div>
-    <modal-primary v-model="valor" />
-  </div>
+  <div />
 </template>
 <script>
-import ModalPrimary from '../components/ModalPrimary.vue'
 export default {
-  components: { ModalPrimary },
   data() {
     return {
-      valor: true,
     };
-  },
-  methods: {
-    teste() {
-      console.log(this.valor);
-    },
   },
 };
 </script>

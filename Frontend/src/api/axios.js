df7388c705d6e66924e504c87961a43f58f5e2e8
Felipe Janotte <f.a.janotte@gmail.com>
2023-09-06 19:40:17 -0300

    fix/login-view

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import Toastify from 'toastify-js';
 
+axios.defaults.baseURL = import.meta.env.VITE_API_URL_DEV
+
 axios.interceptors.response.use((response) => {
   if (
     response.status >= 200 &&
@@ -29,29 +31,31 @@ axios.interceptors.response.use((response) => {
         y: 65,
       },
     }).showToast();
-  } else if (response.status >= 400 && response.status < 600) {
-    Toastify({
-      avatar: '/public/x-circle-fill.svg',
-      text: response.data.message,
-      duration: 3000,
-      gravity: 'top',
-      position: 'right',
-      style: {
-        background:
-          'linear-gradient(90deg, var(--others-red-600) 0%, var(--others-red-300) 100%)',
-        color: 'white',
-        boxShadow:
-          '0px 0px 5px -16px var(--others-red-600), 5px 5px 36px -9px var(--others-red-300)',
-        display: 'flex',
-        alignItems: 'center',
-        gap: '.25rem',
-      },
-      offset: {
-        x: 0,
-        y: 65,
-      },
-    }).showToast();
   }
+  return response;
+}, ({ response }) => {
+  Toastify({
+    avatar: '/public/x-circle-fill.svg',
+    text: response.data.message,
+    duration: 3000,
+    gravity: 'top',
+    position: 'right',
+    style: {
+      background:
+        'linear-gradient(90deg, var(--others-red-600) 0%, var(--others-red-300) 100%)',
+      color: 'white',
+      boxShadow:
+        '0px 0px 5px -16px var(--others-red-600), 5px 5px 36px -9px var(--others-red-300)',
+      display: 'flex',
+      alignItems: 'center',
+      gap: '.25rem',
+    },
+    offset: {
+      x: 0,
+      y: location.href.includes('login') ? 0 : 65,
+    },
+  }).showToast();
+
   return response;
 });
 

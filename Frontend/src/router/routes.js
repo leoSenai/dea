0a71248498b0339b475e7f39f55b031c1526dd6e
dariokrugerjunior <krugerdario1@gmail.com>
2023-10-16 19:31:36 -0300

    fix: correções estrutura de pastas

--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,7 +1,7 @@
 import Home from '../views/HomeView.vue';
 import Login from '../views/LoginView.vue';
 import Header from '../components/HeaderPrimary.vue';
-import Quiz from '../views/QuizView.vue';
+import Quiz from '../views/quiz/QuizView.vue';
 import Users from '../views/users/UsersView.vue'
 import NotFound from '../views/NotFoundView.vue';
 import { PhUsers, PhArticle } from '@phosphor-icons/vue';

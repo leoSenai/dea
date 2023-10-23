0d7883b3d767337be97ecc5f99ac6bd84ce3f95e
dariokrugerjunior <krugerdario1@gmail.com>
2023-10-23 20:14:48 -0300

    fix: implementação para verificar tabelas pessoas e pacientes para fazer a conexão

--- Backend/utils/CryptUtils.go
+++ Backend/utils/CryptUtils.go
@@ -36,33 +36,61 @@ func GenerateRandomPassword(length int) (string, error) {
 	return string(randomBytes), nil
 }
 
-func IsPasswordValid(login string, password string) (isValid bool) {
+func IsPasswordValid(login string, password string) bool {
 	conn, err := db.GetDB()
 	if err != nil {
-		return
+		return false
 	}
+
+	var userDynamic UserDynamic
+	var userFound, personFound, patientFound bool
 
 	var userVerify models.User
 	userVerify.Password = password
 	userVerify.Name = login
 
 	var user models.User
-	conn.First(&user, "email = ? OR telefone = ?", userVerify.Name, userVerify.Name)
+	var person models.Person
+	var patient models.Patient
+
+	userFound = conn.First(&user, "email = ? OR telefone = ?", userVerify.Name, userVerify.Name).Error == nil
+	if !userFound {
+		personFound = conn.First(&person, "email = ? OR telefone = ?", userVerify.Name, userVerify.Name).Error == nil
+		if !personFound {
+			patientFound = conn.First(&patient, "email = ? OR telefone = ?", userVerify.Name, userVerify.Name).Error == nil
+			if !patientFound {
+				return false
+			}
+			userDynamic = UserDynamic{
+				Email:    patient.Email,
+				Phone:    patient.Phone,
+				Password: patient.Password,
+				Salt:     patient.Salt,
+			}
+		} else {
+			userDynamic = UserDynamic{
+				Email:    person.Email,
+				Phone:    person.Phone,
+				Password: person.Password,
+				Salt:     person.Salt,
+			}
+		}
+	} else {
+		userDynamic = UserDynamic{
+			Email:    user.Email,
+			Phone:    user.Phone,
+			Password: user.Password,
+			Salt:     user.Salt,
+		}
+	}
 
 	sha256o := sha256.New()
-	passwordSalted := userVerify.Password + user.Salt
+	passwordSalted := userVerify.Password + userDynamic.Salt
 
 	sha256o.Write([]byte(passwordSalted))
 	encryptedPassword := hex.EncodeToString(sha256o.Sum(nil))
 
-	print("Teste: 1 " + user.Password)
-	print("Teste: 2 " + encryptedPassword)
-	if encryptedPassword == user.Password {
-		return true
-	} else {
-		return false
-	}
-
+	return encryptedPassword == userDynamic.Password
 }
 
 func generateSalt() (salt string) {
@@ -72,4 +100,11 @@ func generateSalt() (salt string) {
 	salt = hex.EncodeToString(saltByte)
 
 	return salt
+}
+
+type UserDynamic struct {
+	Email    string
+	Phone    string
+	Password string
+	Salt     string
 }

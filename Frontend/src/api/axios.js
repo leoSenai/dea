2fe2319a38acb0ddd271f7e6c3d7e2417a48f1d0
Rodrigo Perozin <rodrigodperozin@gmail.com>
2023-10-22 23:28:42 -0300

    fix: toastfy login success alert box offset

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -39,7 +39,7 @@ axios.interceptors.response.use((response) => {
       },
       offset: {
         x: 0,
-        y: location.href.includes('login') ? 0 : 65,
+        y: 70,
       },
     }).showToast();
   }

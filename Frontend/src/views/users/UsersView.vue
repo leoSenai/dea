79b7919dc3f8a0b8985490b3412f16e60383a795
Felipe Janotte <f.a.janotte@gmail.com>
2023-10-23 20:41:09 -0300

    fix/styles

--- Frontend/src/views/users/UsersView.vue
+++ Frontend/src/views/users/UsersView.vue
@@ -58,139 +58,139 @@
     @close="load"
   />
 </template>
-  <script>
-  import { PhPlus, PhPencil } from '@phosphor-icons/vue';
-  import UsersAddEditModal from './UsersAddEditModal.vue';
-  
-  export default {
-    components: {
-      PhPlus,
-      PhPencil,
-      UsersAddEditModal
-    },
-    data() {
-      return {
-        model: {
-          data: [],
-          hasError: false,
-          message: ''
-        },
-      }
-    },
-    computed: {
-      isMobile() {
-        return this.$q.screen.xs || this.$q.screen.sm
-      }
-    },
-    mounted() {
-      this.load();
-    },
-    methods: {
-      load() {
-        const th = this;
-        th.$api.UsersController.getAll().then(({ data }) => {
-          th.model = data
-        }).catch(({ response }) => {
-          th.model = {
-            ...response.data,
-            hasError: true
-          }
-        })
+<script>
+import { PhPlus, PhPencil } from '@phosphor-icons/vue';
+import UsersAddEditModal from './UsersAddEditModal.vue';
+
+export default {
+  components: {
+    PhPlus,
+    PhPencil,
+    UsersAddEditModal
+  },
+  data() {
+    return {
+      model: {
+        data: [],
+        hasError: false,
+        message: ''
       },
-      openAddEditModal(current) {
-        this.$refs.addEdit.openModal(current)
-      }
+    }
+  },
+  computed: {
+    isMobile() {
+      return this.$q.screen.xs || this.$q.screen.sm
+    }
+  },
+  mounted() {
+    this.load();
+  },
+  methods: {
+    load() {
+      const th = this;
+      th.$api.UsersController.getAll().then(({ data }) => {
+        th.model = data
+      }).catch(({ response }) => {
+        th.model = {
+          ...response.data,
+          hasError: true
+        }
+      })
     },
-  }
-  </script>
-  <style>
-  .user-content {
-    padding: 3rem 1.5rem;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    gap: .75rem;
-  }
-  
-  .user-title {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  
-  .title-add-user button {
-    background: var(--primary);
-    border-radius: 8px;
-    border: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: .2s;
-    gap: .5rem;
-    padding: .5rem 1rem;
-    cursor: pointer;
-  }
-  
-  .title-add-user button:hover {
-    filter: brightness(0.8);
-  }
-  
-  .error {
-    border: 1px solid var(--neutral-dark-gray);
-    border-radius: 4px;
-    padding: 1rem;
-  }
-  
-  .add-user {
-    position: absolute;
-    bottom: 1rem;
-    right: 1rem;
-  }
-  
-  .add-user button {
-    background: var(--primary);
-    border-radius: 99999px;
-    font-size: 2rem;
-    border: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 3rem;
-    height: 3rem;
-    transition: .2s;
-  }
-  
-  .add-user button:hover {
-    filter: brightness(0.8);
-    cursor: pointer;
-  }
-  
-  .user {
-    border: 1px solid;
-    padding: 1rem;
-    border-radius: 4px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  .user p {
-    margin: 0;
-  }
-  
-  .user button {
-    border: none;
-    background: none;
-    cursor: pointer;
-    padding: .5rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: .1s;
-    border-radius: 9999px;
-  }
-  
-  .user button:hover {
-    background: var(--neutral-dark-gray);
-  }
-  </style>
+    openAddEditModal(current) {
+      this.$refs.addEdit.openModal(current)
+    }
+  },
+}
+</script>
+<style scoped>
+.user-content {
+  padding: 3rem 1.5rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: .75rem;
+}
+
+.user-title {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.title-add-user button {
+  background: var(--primary);
+  border-radius: 8px;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: .2s;
+  gap: .5rem;
+  padding: .5rem 1rem;
+  cursor: pointer;
+}
+
+.title-add-user button:hover {
+  filter: brightness(0.8);
+}
+
+.error {
+  border: 1px solid var(--neutral-dark-gray);
+  border-radius: 4px;
+  padding: 1rem;
+}
+
+.add-user {
+  position: absolute;
+  bottom: 1rem;
+  right: 1rem;
+}
+
+.add-user button {
+  background: var(--primary);
+  border-radius: 99999px;
+  font-size: 2rem;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 3rem;
+  height: 3rem;
+  transition: .2s;
+}
+
+.add-user button:hover {
+  filter: brightness(0.8);
+  cursor: pointer;
+}
+
+.user {
+  border: 1px solid;
+  padding: 1rem;
+  border-radius: 4px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.user p {
+  margin: 0;
+}
+
+.user button {
+  border: none;
+  background: none;
+  cursor: pointer;
+  padding: .5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: .1s;
+  border-radius: 9999px;
+}
+
+.user button:hover {
+  background: var(--neutral-dark-gray);
+}
+</style>
